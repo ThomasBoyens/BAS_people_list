8d56e2f7a9bb73b89a286858c56a2bcf311a2df0
Thomas Boyens <s115606@ap.be>
2022-06-16 10:42:43 +0000

    json

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -22,15 +22,17 @@ sap.ui.define([
             // detail page is busy indication immediately so there is no break in
             // between the busy indication for loading the view's meta data
             var oViewModel = new JSONModel({
-                busy : false,
-                delay : 0
+                busy: false,
+                delay: 0
             });
 
             this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
 
             this.setModel(oViewModel, "detailView");
 
-            this.getOwnerComponent().getModel().metadataLoaded().then(this._onMetadataLoaded.bind(this));
+            // this.getOwnerComponent().getModel().metadataLoaded().then(this._onMetadataLoaded.bind(this));
+            const oModel = new sap.ui.model.json.JSONModel();
+            this.getView().setModel(oModel, 'json');
         },
 
         /* =========================================================== */
@@ -51,7 +53,16 @@ sap.ui.define([
             );
         },
 
-        
+        onSave: function (oEvent) {
+            const oPerson = this.getView().getBindingContext().getObject();
+            this.getModel().update(`/PersonSet(${oPerson.Id})`, oPerson,
+                {
+                    succes: function (oFeedback) { console.log(oFeedback); },
+                    error: function (oError) { console.error(oError); }
+                });
+        },
+
+
 
         /* =========================================================== */
         /* begin: internal methods                                     */
@@ -64,85 +75,89 @@ sap.ui.define([
          * @private
          */
         _onObjectMatched: function (oEvent) {
-            var sObjectId =  oEvent.getParameter("arguments").objectId;
+            // var sObjectId =  oEvent.getParameter("arguments").objectId;
+            const sObjectId = oEvent.getParameter("arguments").objectId;
             this.getModel("appView").setProperty("/layout", "TwoColumnsMidExpanded");
-            this.getModel().metadataLoaded().then( function() {
-                var sObjectPath = this.getModel().createKey("PersonSet", {
-                    Id:  sObjectId
-                });
-                this._bindView("/" + sObjectPath);
-            }.bind(this));
-        },
+            //     this.getModel().metadataLoaded().then( function() {
+            //         var sObjectPath = this.getModel().createKey("PersonSet", {
+            //             Id:  sObjectId
+            //         });
+            //         this._bindView("/" + sObjectPath);
+            //     }.bind(this));
+            // },
 
-        /**
-         * Binds the view to the object path. Makes sure that detail view displays
-         * a busy indicator while data for the corresponding element binding is loaded.
-         * @function
-         * @param {string} sObjectPath path to the object to be bound to the view.
-         * @private
-         */
-        _bindView: function (sObjectPath) {
-            // Set busy indicator during view binding
-            var oViewModel = this.getModel("detailView");
+            // /**
+            //  * Binds the view to the object path. Makes sure that detail view displays
+            //  * a busy indicator while data for the corresponding element binding is loaded.
+            //  * @function
+            //  * @param {string} sObjectPath path to the object to be bound to the view.
+            //  * @private
+            //  */
+            // _bindView: function (sObjectPath) {
+            //     // Set busy indicator during view binding
+            //     var oViewModel = this.getModel("detailView");
 
-            // If the view was not bound yet its not busy, only if the binding requests data it is set to busy again
-            oViewModel.setProperty("/busy", false);
+            //     // If the view was not bound yet its not busy, only if the binding requests data it is set to busy again
+            //     oViewModel.setProperty("/busy", false);
 
-            this.getView().bindElement({
-                path : sObjectPath,
-                events: {
-                    change : this._onBindingChange.bind(this),
-                    dataRequested : function () {
-                        oViewModel.setProperty("/busy", true);
-                    },
-                    dataReceived: function () {
-                        oViewModel.setProperty("/busy", false);
-                    }
-                }
-            });
-        },
+            //     this.getView().bindElement({
+            //         path : sObjectPath,
+            //         events: {
+            //             change : this._onBindingChange.bind(this),
+            //             dataRequested : function () {
+            //                 oViewModel.setProperty("/busy", true);
+            //             },
+            //             dataReceived: function () {
+            //                 oViewModel.setProperty("/busy", false);
+            //             }
+            //         }
+            //     });
+            // },
 
-        _onBindingChange: function () {
-            var oView = this.getView(),
-                oElementBinding = oView.getElementBinding();
+            // _onBindingChange: function () {
+            //     var oView = this.getView(),
+            //         oElementBinding = oView.getElementBinding();
 
-            // No data for the binding
-            if (!oElementBinding.getBoundContext()) {
-                this.getRouter().getTargets().display("detailObjectNotFound");
-                // if object could not be found, the selection in the list
-                // does not make sense anymore.
-                this.getOwnerComponent().oListSelector.clearListListSelection();
-                return;
+            //     // No data for the binding
+            //     if (!oElementBinding.getBoundContext()) {
+            //         this.getRouter().getTargets().display("detailObjectNotFound");
+            //         // if object could not be found, the selection in the list
+            //         // does not make sense anymore.
+            //         this.getOwnerComponent().oListSelector.clearListListSelection();
+            //         return;
+            this.getModel('json').setData(this.getOwnerComponent()._type);
+            if (!this.getOwnerComponent()._type || !this.getOwnerComponent()._type.TypeName || sObjectId !== this.getOwnerComponent()._type.TypeName) {
+                this.getRouter().navTo('list', {}, true)
             }
 
-            var sPath = oElementBinding.getPath(),
-                oResourceBundle = this.getResourceBundle(),
-                oObject = oView.getModel().getObject(sPath),
-                sObjectId = oObject.Id,
-                sObjectName = oObject.Firstname,
-                oViewModel = this.getModel("detailView");
+        //     var sPath = oElementBinding.getPath(),
+        //         oResourceBundle = this.getResourceBundle(),
+        //         oObject = oView.getModel().getObject(sPath),
+        //         sObjectId = oObject.Id,
+        //         sObjectName = oObject.Firstname,
+        //         oViewModel = this.getModel("detailView");
 
-            this.getOwnerComponent().oListSelector.selectAListItem(sPath);
+        //     this.getOwnerComponent().oListSelector.selectAListItem(sPath);
 
-            oViewModel.setProperty("/shareSendEmailSubject",
-                oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
-            oViewModel.setProperty("/shareSendEmailMessage",
-                oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
-        },
+        //     oViewModel.setProperty("/shareSendEmailSubject",
+        //         oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
+        //     oViewModel.setProperty("/shareSendEmailMessage",
+        //         oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
+        // },
 
-        _onMetadataLoaded: function () {
-            // Store original busy indicator delay for the detail view
-            var iOriginalViewBusyDelay = this.getView().getBusyIndicatorDelay(),
-                oViewModel = this.getModel("detailView");
+        // _onMetadataLoaded: function () {
+        //     // Store original busy indicator delay for the detail view
+        //     var iOriginalViewBusyDelay = this.getView().getBusyIndicatorDelay(),
+        //         oViewModel = this.getModel("detailView");
 
-            // Make sure busy indicator is displayed immediately when
-            // detail view is displayed for the first time
-            oViewModel.setProperty("/delay", 0);
+        //     // Make sure busy indicator is displayed immediately when
+        //     // detail view is displayed for the first time
+        //     oViewModel.setProperty("/delay", 0);
 
-            // Binding the view will set it to not busy - so the view is always busy if it is not bound
-            oViewModel.setProperty("/busy", true);
-            // Restore original busy indicator delay for the detail view
-            oViewModel.setProperty("/delay", iOriginalViewBusyDelay);
+        //     // Binding the view will set it to not busy - so the view is always busy if it is not bound
+        //     oViewModel.setProperty("/busy", true);
+        //     // Restore original busy indicator delay for the detail view
+        //     oViewModel.setProperty("/delay", iOriginalViewBusyDelay);
         },
 
         /**
@@ -167,7 +182,7 @@ sap.ui.define([
                 this.getModel("appView").setProperty("/layout", "MidColumnFullScreen");
             } else {
                 // reset to previous layout
-                this.getModel("appView").setProperty("/layout",  this.getModel("appView").getProperty("/previousLayout"));
+                this.getModel("appView").setProperty("/layout", this.getModel("appView").getProperty("/previousLayout"));
             }
         }
     });
